f4f0e5aa86405fd0a88fd3e379e31ec39d4a6a0e
yzd <yzd01234567890@163.com>
2026-08-09 15:42:51 +0800

    初始化项目

---
+++ src/test/java/com/yang/yaiagent/YAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yang.yaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
