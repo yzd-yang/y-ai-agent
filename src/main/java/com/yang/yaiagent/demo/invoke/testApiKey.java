c55e0a54a6ed00d5caf7d7f770009bd63cb1040e
yzd <yzd01234567890@163.com>
2026-08-10 22:35:52 +0800

    脱敏

--- src/main/java/com/yang/yaiagent/demo/invoke/testApiKey.java
+++ src/main/java/com/yang/yaiagent/demo/invoke/testApiKey.java
@@ -2,5 +2,5 @@ package com.yang.yaiagent.demo.invoke;
 
 public interface testApiKey {
 
-    String API_KEY="sk-ws-H.ERPERII.eMNH.MEUCIQD27cqoTbPIUWz4HBZEhMIiR-snHIeFMhF4fhBBIWKecgIgbTKdUucyXDoWzSMlPIU19TBdUy7Sw5s_BpKKXmpHVsk";
+    String API_KEY="YOU_KEY";
 }
